cece3929aa4ebc06208d8d86174ad57c01920696
Kristo <christo.losier@gmail.com>
2026-06-23 13:09:36 +0200

    test: restore registry count coverage

--- src/data/collection-manager.test.js
+++ src/data/collection-manager.test.js
@@ -1,6 +1,8 @@
 import test from "node:test";
 import assert from "node:assert/strict";
 
-test("test harness runs", () => {
-  assert.ok(true);
+const mod = await import("./collections.js");
+
+test("registry count", () => {
+  assert.equal(mod.collections.length, 9);
 });
